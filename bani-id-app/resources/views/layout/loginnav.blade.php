19259a7ec3b2acacaa1c19cb8f79528815e07891
deeja-ub <tomeldendjaninetara@gmail.com>
2024-04-09 22:44:44 +0800

    Login form and dashboard
    -Layout for Login form
    - Layout for dashboard

--- bani-id-app/resources/views/layout/loginnav.blade.php
+++ bani-id-app/resources/views/layout/loginnav.blade.php
@@ -1,1 +1,13 @@
-test
+<nav>
+    <ul class="nav-links">
+        <li>
+            <a href="">TEST</a>
+        </li>
+        <li>
+            <a href="">TEST</a>
+        </li>
+        <li>
+            <a href="">TEST</a>
+        </li>
+    </ul>
+</nav>
