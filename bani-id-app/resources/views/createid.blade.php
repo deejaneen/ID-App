8ec37449c3f6f47ad87a7e24d2c7edc48169c69c
deejacodes <tomeldendjaninetara@gmail.com>
2024-04-24 17:13:11 +0800

    Fixed Layouts

--- bani-id-app/resources/views/createid.blade.php
+++ bani-id-app/resources/views/createid.blade.php
@@ -31,123 +31,209 @@
         {{-- END OF ASIDE --}}
         <main>
             <div class="create-id-container">
-                <div class="button-section">
-                    <button class="btn-create btn btn-primary btn-lg rounded-pill">
-                        Create
-                    </button>
-                    <button class="btn-edit btn btn-primary btn-lg rounded-pill">
-                        Edit
-                    </button>
-                </div>
-                <div class="department-course-section">
-                    <div class="dcs-department">
-                        <h3 class="input-label">Department</h3>
-                        <div class="dropdown-center department">
-                            <button id="dropdownToggle" class="btn btn-secondary dropdown-toggle" type="button"
-                                data-bs-toggle="dropdown" aria-expanded="false" style="min-width: 100%;">
-                                Department
-                            </button>
-                            <ul class="dropdown-menu" style="min-width: 100%;">
-                                <li><a class="dropdown-item" href="javascript:void(0);"
-                                        data-value="COA Employee">Test1</a></li>
-                                <li><a class="dropdown-item" href="javascript:void(0);"
-                                        data-value="Non - COA">Test2</a></li>
-                            </ul>
-                        </div>
-                    </div>
-                    <div class="dcs-course">
-                        <h3 class="input-label">Course</h3>
-                        <div class="dropdown-center course">
-                            <button id="dropdownToggle" class="btn btn-secondary dropdown-toggle" type="button"
-                                data-bs-toggle="dropdown" aria-expanded="false" style="min-width: 100%;">
-                                Course
-                            </button>
-                            <ul class="dropdown-menu" style="min-width: 100%;">
-                                <li><a class="dropdown-item" href="javascript:void(0);"
-                                        data-value="COA Employee">Test1</a></li>
-                                <li><a class="dropdown-item" href="javascript:void(0);"
-                                        data-value="Non - COA">Test2</a></li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-                <div class="id-section">
-                    <div class="slsu-section">
-                        <h5 class="fname">SLSU</h5>
-                        <small class=""></small>
-                    </div>
-                    <div class="name-section">
-                        <h5 class="fname">First Name</h5>
-                        <small class="fname">First Name</small>
-                        <h5 class="mname">Middle Name</h5>
-                        <small class="mname">Middle Name</small>
-                        <h5 class="lname">Last Name</h5>
-                        <small class="lname">Last Name</small>
-                        <h5 class="suffix">Suffix</h5>
-                        <small class="suffix">Suffix</small>
-                    </div>
-                    <div class="id-number-section">
-                        <h5 class="idnumber">ID Number</h5>
-                        <small class="idnumber">ID Number</small>
-                    </div>
-                    <div class="department-section">
-                        <h5 class="department">Department</h5>
-                        <small class="department">Department</small>
-                    </div>
-                    <div class="course-section">
-                        <h5 class="course">Course</h5>
-                        <small class="course">Course</small>
-                    </div>
-                </div>
-                <div class="id-info">
-                    <div class="id-info-name-section">
-                        <div class="input-fname">
-                            <h3 class="input-label">First Name</h3>
-                            <input type="text">
-                        </div>
-                        <div class="input-mname">
-                            <h3 class="input-label">Middle Name</h3>
-                            <input type="text">
-                        </div>
-                        <div class="input-lname">
-                            <h3 class="input-label">Last Name</h3>
-                            <input type="text">
-                        </div>
-                        <div class="input-suffix">
-                            <h3 class="input-label">Suffix</h3>
-                            <input type="text">
-                        </div>
-                    </div>
-                    <div class="id-info-number-section">
-                        <div class="input">
-                            <h3 class="input-label">ID Number</h3>
-                            <input type="text">
-                        </div>
-                    </div>
-                    <div class="id-info-department-section">
-                        <div class="input">
-                            <h3 class="input-label">Department</h3>
-                            <input type="text" disabled>
-                        </div>
-                    </div>
-                    <div class="id-info-course-section">
-                        <div class="input">
-                            <h3 class="input-label">Course</h3>
-                            <input type="text" disabled>
-                        </div>
-                    </div>
-                    <div class="id-info-button-section">
-                        <button class="btn-create btn btn-primary btn-lg rounded-pill">
+       
+                <div class="card-container" id="createCard">
+                    <div class="button-section">
+                        <button class="btn-create btn btn-primary btn-lg rounded-pill toogle-btn"  id="createTogglebtn" data-mdb-ripple-init>
+                            <span class="fa-solid fa-repeat"></span>
                             Create
                         </button>
-                        <button class="btn-create btn btn-primary btn-lg rounded-pill">
-                            Clear
-                        </button>
+                        {{-- <button class="btn-edit btn btn-primary btn-lg rounded-pill">
+                            Edit
+                        </button> --}}
                     </div>
-                    <h2 class="id-info-label">ID Information</h2>
+                    <div class="department-course-section">
+                        <div class="dcs-department">
+                            <h3 class="input-label">Department</h3>
+                            <div class="dropdown-center department">
+                                <button id="dropdownToggle" class="btn btn-secondary dropdown-toggle" type="button"
+                                    data-bs-toggle="dropdown" aria-expanded="false" style="min-width: 100%;">
+                                    Department
+                                </button>
+                                <ul class="dropdown-menu" style="min-width: 100%;">
+                                    <li><a class="dropdown-item" href="javascript:void(0);"
+                                            data-value="COA Employee">Test1</a></li>
+                                    <li><a class="dropdown-item" href="javascript:void(0);" data-value="Non - COA">Test2</a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div class="dcs-course">
+                            <h3 class="input-label">Course</h3>
+                            <div class="dropdown-center course">
+                                <button id="dropdownToggle" class="btn btn-secondary dropdown-toggle" type="button"
+                                    data-bs-toggle="dropdown" aria-expanded="false" style="min-width: 100%;">
+                                    Course
+                                </button>
+                                <ul class="dropdown-menu" style="min-width: 100%;">
+                                    <li><a class="dropdown-item" href="javascript:void(0);"
+                                            data-value="COA Employee">Test1</a></li>
+                                    <li><a class="dropdown-item" href="javascript:void(0);" data-value="Non - COA">Test2</a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="id-section">
+                        <div class="slsu-section">
+                            <h5 class="fname">SLSU</h5>
+                            <small class=""></small>
+                        </div>
+                        <div class="name-section">
+                            <h5 class="fname">First Name</h5>
+                            <small class="fname">First Name</small>
+                            <h5 class="mname">Middle Name</h5>
+                            <small class="mname">Middle Name</small>
+                            <h5 class="lname">Last Name</h5>
+                            <small class="lname">Last Name</small>
+                            <h5 class="suffix">Suffix</h5>
+                            <small class="suffix">Suffix</small>
+                        </div>
+                        <div class="id-number-section">
+                            <h5 class="idnumber">ID Number</h5>
+                            <small class="idnumber">ID Number</small>
+                        </div>
+                        <div class="department-section">
+                            <h5 class="department">Department</h5>
+                            <small class="department">Department</small>
+                        </div>
+                        <div class="course-section">
+                            <h5 class="course">Course</h5>
+                            <small class="course">Course</small>
+                        </div>
+                    </div>
+                    <div class="id-info">
+                        <div class="id-info-name-section">
+                            <div class="input-fname">
+                                <h3 class="input-label">First Name</h3>
+                                <input type="text">
+                            </div>
+                            <div class="input-mname">
+                                <h3 class="input-label">Middle Name</h3>
+                                <input type="text">
+                            </div>
+                            <div class="input-lname">
+                                <h3 class="input-label">Last Name</h3>
+                                <input type="text">
+                            </div>
+                            <div class="input-suffix">
+                                <h3 class="input-label">Suffix</h3>
+                                <input type="text">
+                            </div>
+                        </div>
+                        <div class="id-info-number-section">
+                            <div class="input">
+                                <h3 class="input-label">ID Number</h3>
+                                <input type="text">
+                            </div>
+                        </div>
+                        <div class="id-info-department-section">
+                            <div class="input">
+                                <h3 class="input-label">Department</h3>
+                                <input type="text" disabled>
+                            </div>
+                        </div>
+                        <div class="id-info-course-section">
+                            <div class="input">
+                                <h3 class="input-label">Course</h3>
+                                <input type="text" disabled>
+                            </div>
+                        </div>
+                        <div class="id-info-button-section">
+                            <button class="btn-create btn btn-primary btn-lg rounded-pill">
+                                Create
+                            </button>
+                            <button class="btn-create btn btn-primary btn-lg rounded-pill">
+                                Clear
+                            </button>
+                        </div>
+                        <h2 class="id-info-label">ID Information</h2>
+                    </div>
+                </div>
+                <div class="card-container" id="editCard" style="display: none;">
+                    <div class="button-section">
+                        <button class="btn-edit btn btn-primary btn-lg rounded-pill toogle-btn"   id="editTogglebtn" data-mdb-ripple-init>
+                            Edit
+                        </button>
+                        {{-- <button class="btn-edit btn btn-primary btn-lg rounded-pill">
+                            Edit
+                        </button> --}}
+                    </div>
+                    <div class="department-course-section">
+                        <div class="dcs-department">
+                            <h3 class="input-label">Department</h3>
+                            <div class="dropdown-center department">
+                                <button id="dropdownToggle" class="btn btn-secondary dropdown-toggle" type="button"
+                                    data-bs-toggle="dropdown" aria-expanded="false" style="min-width: 100%;">
+                                    Department
+                                </button>
+                                <ul class="dropdown-menu" style="min-width: 100%;">
+                                    <li><a class="dropdown-item" href="javascript:void(0);"
+                                            data-value="COA Employee">Test1</a></li>
+                                    <li><a class="dropdown-item" href="javascript:void(0);"
+                                            data-value="Non - COA">Test2</a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div class="dcs-course">
+                            <h3 class="input-label">Course</h3>
+                            <div class="dropdown-center course">
+                                <button id="dropdownToggle" class="btn btn-secondary dropdown-toggle" type="button"
+                                    data-bs-toggle="dropdown" aria-expanded="false" style="min-width: 100%;">
+                                    Course
+                                </button>
+                                <ul class="dropdown-menu" style="min-width: 100%;">
+                                    <li><a class="dropdown-item" href="javascript:void(0);"
+                                            data-value="COA Employee">Test1</a></li>
+                                    <li><a class="dropdown-item" href="javascript:void(0);"
+                                            data-value="Non - COA">Test2</a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <table class="table-home table-hover" id="EditIDTable" style="width: 100%">
+                        <thead>
+                            <tr>
+                                <th scope="col">Reservation Number</th>
+                                <th scope="col">Reservation Date</th>
+                                <th scope="col">Time Start</th>
+                                <th scope="col">Time End</th>
+                                <th scope="col">Purpose</th>
+                                <th scope="col">Price</th>
+                                <th scope="col">Status</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            
+                                <tr class="table-active">
+                                    
+                                </tr>
+                        </tbody>
+                    </table>
                 </div>
             </div>
         </main>
     </div>
+    <script>
+        document.addEventListener("DOMContentLoaded", function() {
+        const createCard = document.getElementById("createCard");
+        const editCard = document.getElementById("editCard");
+        const btnCreate = document.querySelector(".btn-create");
+        const btnEdit = document.querySelector(".btn-edit");
+    
+        btnCreate.addEventListener("click", function() {
+            createCard.style.display = "grid";
+            editCard.style.display = "none";
+        });
+    
+        btnEdit.addEventListener("click", function() {
+            createCard.style.display = "none";
+            editCard.style.display = "grid";
+        });
+    });
+    
+    </script>
     
 @endsection
--- bani-id-app/resources/views/layout/weblayout.blade.php
+++ bani-id-app/resources/views/layout/weblayout.blade.php
@@ -17,19 +17,63 @@
 
 
 
+
 <body>
     @include('layout.websitenav')
 
-    
+
     @yield('dashboard')
     @yield('adminpanel')
     @yield('createid')
 
     {{-- Stylesheet for font from google fonts --}}
+   
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"
+        integrity="sha512-VEd+nq25CkR676O+pLBnDW09R7VQX9Mdiij052gVCp5yVH3jGtH70Ho/UUv4mJDsEdTvqRCFZg0NKGiojGnUCw=="
+        crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+
+    <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
     <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
     <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.datatables.net/2.0.3/js/dataTables.min.js"></script>
+
+    <script>
+        $(document).ready(function() {
+            $('#createTogglebtn').click(function(e) {
+                e.preventDefault();
+
+                $('#createCard').toggle();
+                $('#editCard').toggle();
+            });
+
+            $('#editTogglebtn').click(function(e) {
+                e.preventDefault();
+
+                $('#editCard').toggle();
+                $('#createCard').toggle();
+
+            });
+        });
+    </script>
+    <script>
+        $(document).ready(function() {
+            $('#EditIDTable').DataTable();
+         
+            // $('#EditIDTable').DataTable({
+            //     "paging": false,
+            //     "pageLength": 5,
+            //     "lengthMenu": [5, 10, 15, 20],
+            //     "columnDefs": [{
+            //         "targets": "_all", // Corrected target to be a string "_all"
+            //         "className": "dt-head-left"
+            //     }],
+
+            // });
+
+        });
     </script>
 </body>
 
